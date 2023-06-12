1536f581db624b639dd8a0efc8d2f64b3f7adc8b
Luis Pérez <luis.perez@sedecal.com>
2023-06-12 15:11:31 +0200

    Update Favorite and Available

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,19 @@
             <div class="col-lg-10 offset-lg-1">
                 <div class="card mt-4">
                      
-                    <div class="input-group">
+                    <div class="input-group" style="align-items: center;">
                         <input type="text"
                             v-model="filtro"
                             class="form-control form-control-lg" 
                             placeholder="Filtrar"
-                            v-on:keyup="FiltrarColor">
+                            v-on:keyup="FiltrarColor"/>
+                        <i id="id-show-me-pictures" v-bind:class="[this.show_mis_pinturas ? 'fas fa-bookmark fa-lg': 'far fa-bookmark fa-lg']" style="margin:10px" v-on:click="ShowMePictures()"></i>                        
                     </div>
                     <br>  
                     <div class="row row-cols-1 row-cols-md-5 g-4">
                         <div class="col" v-for="(color, index) of listColores" :key="index">
-                            <div class="card">
-                                <img v-bind:src="color.imagen" class="img-thumbnail">
+                            <div class="card">                                
+                                <img v-bind:src="color.imagen_large" class="img-thumbnail" style="border: 0; aspect-ratio: 1/1;">
                                 <div class="card-body">
                                     <p class="card-text fw-bold">{{color.nombre}}</p>
                                     <div class="list-group-item d-flex justify-content-between align-items-end">
@@ -51,24 +52,26 @@
 <script>        
     //import { auth, app } from "./utils/firebase";
     import { app } from "./utils/firebase";    
-    import { getDatabase, ref, onValue } from 'firebase/database'
+    import { getDatabase, ref, onValue, update } from 'firebase/database'
 
     const db = getDatabase(app);
     const settingsRef = ref(db, 'Oleo')
-
     export default {
         name: 'ListPinturas',
         mounted(){            
-            onValue(settingsRef, (c) => {                     
+            alert("Cargando Datos");
+            onValue(settingsRef, (c) => {                       
                 c.forEach((childColor) => {
                     this.listColores.push({ 
+                        id: childColor.key,
                         nombre: childColor.child("Nombre").val(),
                         numero: childColor.child("InfoColor").val(),
                         precio:  childColor.child("Precio").val(),
                         favorito: childColor.child("Favorite").val(),
                         disponible: childColor.child("Available").val(),
                         code: childColor.child("Referencia").val(),
-                        imagen: childColor.child("Imagen").val()
+                        imagen: childColor.child("Imagen").val(),
+                        imagen_large: childColor.child("ImagenLarge").val(),
                     })
                 })
             }, 
@@ -77,40 +80,55 @@
             });
 
             this.listTodosColores = this.listColores;
-            console.log('Elementos Guardados', this.listColores.length)
         },
         data(){
             return{
                 filtro: '',
                 listColores: [],
-                listTodosColores : []
+                listTodosColores : [],
+                show_mis_pinturas: false
             }
         },
-        methods:{
-            // agregarColor(){
-            //     const color = {
-            //         nombre: this.color,
-            //         favorito: false,
-            //         disponible: false
-            //     }
-            //     this.listColores.push(color);
-            //     this.color = '';
-            // },
-            FiltrarColor(){
-                console.log("Color Filtrar:",this.filtro);
+        methods:{            
+            FiltrarColor(){                
                 this.listColores = this.listTodosColores.filter(c => c.nombre.includes(this.filtro));                
             },
 
+            ShowMePictures(){                
+                this.show_mis_pinturas = !this.show_mis_pinturas;                
+                if (this.show_mis_pinturas){
+                    this.listColores = this.listTodosColores.filter(c => c.nombre.includes(this.filtro) && c.disponible);                        
+                }else{                    
+                    this.listColores = this.listTodosColores;                    
+                }                                
+            },
+           
             QuitarFiltrarColor(){
                 this.listColores = this.listTodosColores;
             },
 
-            CheckAsMine(color, index){
-                this.listColores[index].disponible = !color.disponible;
+            CheckAsMine(color, index){                
+                var _disponible = !color.disponible;
+                                
+                this.listColores[index].disponible = _disponible; 
+                const db = getDatabase();
+                
+                const updates = {};                
+                updates['/Oleo/' + color.id + '/Available'] = _disponible;
+                                
+                update(ref(db), updates);
             },
 
             CheckAsFavorite(color, index){
-                this.listColores[index].favorito = !color.favorito;
+                var _favorite = !color.favorito;
+                this.listColores[index].favorito = _favorite;
+                const db = getDatabase();
+                
+                const updates = {};                
+                updates['/Oleo/' + color.id + '/Favorite'] = _favorite;
+
+                this.listColores = [];
+                update(ref(db), updates);
             }
         }
     }
